d7912a357f59f5b793bba3025ade395eac155aba
Gustavo <gusta.front@gmail.com>
2022-07-02 11:20:51 -0300

    Useless files removed and others renamed

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-// ********** QUERY SELECTORS AND VARIABLES **********
+//  **** OBJETIVO FINAL: criar uma calculadora
+
+//  **** RESTRIÇÕES:
+//  terá apenas as seguintes operações: +, -, * e /
+//  calcular dois pares de números por vez
+//  deverá arredondar números que não são inteiros para uma casa decimal
+
+// **** BÔNUS:
+//  deixar bonita
+//  ter a opção de adicionar casas decimais
+//  adicionar um botão de apagar
+//  ter suporte para teclado
+ 
+
+//  VARIÁVEIS
 let display = document.querySelector('#display');
 let numBtn = document.querySelectorAll('.numbers');
 let opBtn = document.querySelectorAll('.operators');
@@ -7,108 +21,103 @@ let clearBtn = document.querySelector('#clear');
 let percentBtn = document.querySelector('#percentage');
 let rmvBtn = document.querySelector('#remove');
 let decBtn = document.querySelector('#decimal');
-let displayValue = display.textContent;
-let thereIsAlreadyAnOperator = false;
+//  Essa variável servirá para determinar o num2 (retiraremos tudo que veio antes do operador e, portanto, o número 2 será o que sobrar)
+let toGetNum2;
+let displayValue;
 let operator;
-let text;
 let num1;
 let num2;
 let finalResult;
-let lastCharacter;
-let removedText;
+let removedText = '';
 
-//  For each operator button, a mathematical function (CHECK)
+//  FUNÇÕES
 function sum(num1, num2) {
-    finalResult = Number(num1) + Number(num2);
-    if (!(Number.isInteger(finalResult))) {
-        finalResult = finalResult.toFixed(1);
-    }
+    finalResult = num1 + num2;
+    roundIfNotInterger()
 };
 
 function subtr(num1, num2) {
-    finalResult = Number(num1) - Number(num2);
-    if (!(Number.isInteger(finalResult))) {
-        finalResult = finalResult.toFixed(1);
-    }
+    finalResult = num1 - num2;
+    roundIfNotInterger()
 };
 
 function multiply(num1, num2) {
-    finalResult = Number(num1) * Number(num2);
-    if (!(Number.isInteger(finalResult))) {
-        finalResult = finalResult.toFixed(1);
-    }
+    finalResult = num1 * num2;
+    roundIfNotInterger()
 };
 
 function divide(num1, num2) {
-    if (num1 === '0' && num2 === '0' || num2 === '0') {
-        finalResult = 'I don\'t know';
-    } else {
-        finalResult = Number(num1) / Number(num2);
-        if (!(Number.isInteger(finalResult))) {
-            finalResult = finalResult.toFixed(1);
-        }
+    if (num2 === 0) {
+        return finalResult = 'IDK :)';
     }
+    finalResult = num1 / num2;
+    roundIfNotInterger()
 };
 
-//  A function that takes an operator and two numbers and then calls one of the mathematical functions based on the operator
 function operate(operator, num1, num2) {
-    if (operator === '+') sum(num1, num2);
-    if (operator === '-') subtr(num1, num2);
-    if (operator === '*') multiply(num1, num2);
-    if (operator === '/') divide(num1, num2);
-    display.textContent = finalResult;
-};
-
-// ********** FUNCTIONS **********
-//  Create the functions that populate the display when you click the number buttons
-
-function numShowDisplay(button) {
-    if (display.textContent === '0') {
-        display.textContent = button.textContent;
+    if (num2 === '') {
+        return finalResult = num1
     } else {
-        display.textContent += button.textContent; 
+        num1 = Number(num1);
+        num2 = Number(num2);
     }
+    if (operator === '+') return sum(num1, num2);
+    else if (operator === '-') return subtr(num1, num2);
+    else if (operator === '*') return multiply(num1, num2);
+    else return divide(num1, num2);
 };
 
-function getOperator() {
-    // if the final character in the string(.length - 1)) be equal to any of the the operators, stores it on the operator var
-    for (let i = 0; i < opBtn.length; i++) {
-        if (display.textContent[(display.textContent.length - 1)] === opBtn[i].innerText) {
-            operator = opBtn[i].innerText;
-        }
-    }
-};
-
-function getNum1() {
-    // Stores all the numbers besides the operator to num1 var
-    if (finalResult !== undefined) {
-        num1 = finalResult
-    } else {
-        num1 = displayValue;
-    }
-};
-
-function getNum2() {
-        num2 = display.textContent.replace(text, '')
-};
-
-function limitTheNumOfOperators(button) {
-    if (checkIfLastCharacterIsOp()) {
-        thereIsAlreadyAnOperator = true;
-    } else {
-        display.textContent += button.textContent
-        thereIsAlreadyAnOperator = true;
+function roundIfNotInterger() {
+    if (!Number.isInteger(finalResult)) {
+        finalResult = finalResult.toFixed(1);
     }
 }
 
-function checkIfLastCharacterIsOp() {
-    lastCharacter = display.textContent[(display.textContent.length - 1)]
+function displayNum(button) {
+    if (display.textContent === '0') {
+        display.textContent = button.textContent;
+    } else {
+        display.textContent += button.textContent;
+    }
+    displayValue = display.textContent;
+};
+
+function getNum1() {
+    num1 = displayValue;
+};
+
+function getNum2() {
+    num2 = display.textContent.replace(toGetNum2, '');
+};
+
+function getTheFinalCharacter() {
+    return display.textContent[display.textContent.length - 1]
+}
+
+function limitOperator() {
     for (let i = 0; i < opBtn.length; i++) {
-        if (lastCharacter === opBtn[i].innerText) {
-            return true
-        } else {
-            continue
+        if (getTheFinalCharacter() === opBtn[i].textContent) {
+            return true;
         }
+    }
+}
+
+function afterOperation() {
+    num1 = finalResult;
+    operator = undefined;
+    num2 = undefined;
+    toGetNum2 = undefined;
+}
+
+function resetIfResultIDK() {
+    if (display.textContent === 'IDK :)') {
+        toGetNum2 = undefined;
+        displayValue = undefined;
+        operator = undefined;
+        num1 = undefined;
+        num2 = undefined;
+        finalResult = undefined;
+        display.textContent = '0';
     }
 }
 
@@ -117,26 +126,25 @@ function checkIfNum2HaveAlreadyADot(tempText) {
         if (tempText[i] === decBtn.textContent) {
             return true
         }
-        false
     }
 }
 
+function checkIfNum2IsEmpty(tempText) {
+        if (tempText.length = 0) {
+            return true
+        }
+}
+
 function putNum2ADot () {
-    if (text !== undefined) {
-        for (let k = 0; k < display.textContent.length; k++) {
+    if (toGetNum2 !== undefined) {
+        for (let k = 0; k < toGetNum2.length; k++) {
             for (let l = 0; l < opBtn.length; l++) {
-                if (text[k] === opBtn[l].textContent) {
-                    let tempText = display.textContent.replace(text, '')
-                    console.log(tempText)
-                    if (tempText[k] === opBtn[l].textContent) {
+                if (toGetNum2[k] === opBtn[l].textContent) {
+                    let tempText = display.textContent.replace(toGetNum2, '')
+                    if (checkIfNum2HaveAlreadyADot(tempText) || checkIfNum2IsEmpty(tempText)) {
                         return
                     } else {
-                        if (checkIfNum2HaveAlreadyADot(tempText)) {
-                            return
-                        } else {
-                            display.textContent += decBtn.textContent
-                            displayValue = display.textContent
-                        }
+                        display.textContent += decBtn.textContent
                     }
                 }
             }
@@ -144,76 +152,79 @@ function putNum2ADot () {
     }
 }
 
-// ********** EVENTS **********
-//  Create the functions that populate the display when you click the number buttons
+//  EVENTOS
+equalBtn.addEventListener('click', () => {
+    resetIfResultIDK()
+    if (display.textContent.length === 2 && display.textContent[0] === '0') {
+        display.textContent = 0;
+        operator = undefined;
+        toGetNum2 = undefined;
+    }
+    if (num1 === undefined && operator !== undefined) {
+        num1 = '0'
+    }
+    if (display.textContent === '0' || displayValue !== undefined && num1 === undefined || finalResult !== undefined && operator === undefined) {
+        return
+    }
+    getNum2();
+    operate(operator, num1, num2);
+    display.textContent = finalResult;
+    displayValue = finalResult;
+    afterOperation();
+});
+
 numBtn.forEach(button => {
     button.addEventListener('click', () => {
-        numShowDisplay(button);
-        displayValue = display.textContent;
+        resetIfResultIDK();
+        displayNum(button);
     })
 });
 
 opBtn.forEach(button => {
     button.addEventListener('click', () => {
-        // This if will only occour if the user want to display the final result clicking in the operator, not the equal
-          if (num1 !== undefined && operator !== undefined && !(checkIfLastCharacterIsOp())) {
+        resetIfResultIDK()
+        if (num1 === undefined) {
+            num1 = 0
+        }
+        if (limitOperator()) {
+            if (num1 === 0) {
+                operator = button.textContent;
+                display.textContent = num1 + operator;
+                toGetNum2 = display.textContent;
+            } else {
+                getNum1();
+                operator = button.textContent;
+                display.textContent = num1 + operator;
+                toGetNum2 = display.textContent;
+            }
+            return
+        // irá ter o mesmo resultado do 'IGUAL'
+        } else if (!limitOperator() && operator !== undefined) {
             getNum2();
             operate(operator, num1, num2);
-            operator = undefined;
-            num2 = undefined;
-            getNum1();
-            limitTheNumOfOperators(button);
-            getOperator();
-            text = display.textContent;
-            console.log(2);
-        // This if will only occour to get the first number and operator in the first operation
+            display.textContent = finalResult;
+            displayValue = finalResult;
+            afterOperation();
+            operator = button.textContent;
+            display.textContent += operator;
+            toGetNum2 = display.textContent;
         } else {
             getNum1();
-            limitTheNumOfOperators(button);
-            getOperator();
-            text = display.textContent;
-            console.log(3);
+            operator = button.textContent;
+            display.textContent += operator;
+            toGetNum2 = display.textContent;
         }
     })
 });
 
-equalBtn.addEventListener('click', () => {
-    // This if will only occour if the user don't provide a second number, so the result will be just the first number
-    if (num1 !== undefined && operator !== undefined && (checkIfLastCharacterIsOp()) && num2 === undefined) {
-        display.textContent = num1;
-        thereIsAlreadyAnOperator = false;
-        text = undefined;
-        operator = undefined;
-    } else if (finalResult !== undefined && num2 === undefined) {
-        getNum2();
-        operate(operator, num1, num2);
-        operator = undefined;
-        num2 = undefined;
-        console.log('xiiii');
-    } else {
-        lastCharacter = display.textContent[(display.textContent.length - 1)]
-        if (thereIsAlreadyAnOperator === true) {
-            getNum2();
-            operate(operator, num1, num2);
-            getNum1();
-            operator = undefined;
-            num2 = undefined;
-            console.log('xix');
-        }
-    }
-});
-
 clearBtn.addEventListener('click', () => {
-    finalResult = undefined;
+    toGetNum2 = undefined;
+    displayValue = undefined;
     operator = undefined;
-    text = undefined;
     num1 = undefined;
     num2 = undefined;
     finalResult = undefined;
-    lastCharacter = undefined;
-    thereIsAlreadyAnOperator = false;
-    display.textContent = '0'
-    displayValue = display.textContent;
+    display.textContent = '0';
 })
 
 decBtn.addEventListener('click', () => {
@@ -228,24 +239,78 @@ decBtn.addEventListener('click', () => {
             return;
         }
     }
-    display.textContent += decBtn.textContent
-    displayValue = display.textContent
-    console.log(display.textContent[0])
-})
+    display.textContent += decBtn.textContent;
+    displayValue = display.textContent;
+});
 
 rmvBtn.addEventListener('click', () => {
     if (display.textContent === '0') {
         return
-    } else if (display.textContent.length === 1) {
-        display.textContent = '0'
-    } else {
-        removedText = display.textContent.substring((display.textContent.length - 1), (display.textContent.length))
-        display.textContent = display.textContent.replace(removedText, '')
-        for (let i = 0; i < opBtn.length; i++) {
-            if (removedText === opBtn[i].textContent) {
-                operator = undefined
-            }
+    }
+    display.textContent = display.textContent.replace(/\.$|\d$/, '');
+    for (let i = 0; i < opBtn.length; i++) {
+        if (display.textContent[display.textContent.length -1] === opBtn[i].textContent) {
+            return
         }
     }
-    console.log(removedText)
-})
+    displayValue = display.textContent;
+});
+
+//  SUPORTE P/ TECLADO
+window.addEventListener('keydown', (e) => {
+    let code = e.key;
+    if (e.key === '0') {
+        numBtn[9].click();
+    } else if (e.key === '1') {
+        numBtn[0].click();
+    } else if (e.key === '2') {
+        numBtn[1].click();
+    } else if (e.key === '3') {
+        numBtn[2].click();
+    } else if (e.key === '4') {
+        numBtn[3].click();
+    } else if (e.key === '5') {
+        numBtn[4].click();
+    } else if (e.key === '6') {
+        numBtn[5].click();
+    } else if (e.key === '7') {
+        numBtn[6].click();
+    } else if (e.key === '8') {
+        numBtn[7].click();
+    } else if (e.key === '9') {
+        numBtn[8].click();
+    } else if (e.key === '+') {
+        if (e.shiftKey) {
+            opBtn[0].click();
+        } else {
+            opBtn[0].click();
+        }
+    } else if (e.key === '-') {
+        if (e.shiftKey) {
+            opBtn[1].click();
+        }
+        opBtn[1].click()
+    } else if (e.key === '/') {
+        if (e.shiftKey) {
+            opBtn[2].click();
+        }
+        opBtn[2].click()
+    } else if (e.key === '?') {
+        if (e.shiftKey) {
+            opBtn[2].click();
+        }
+    } else if (e.key === '*') {
+        if (e.shiftKey) {
+            opBtn[3].click();
+        }
+        opBtn[3].click();
+    } else if (e.key === 'Enter') {
+        equalBtn.click();
+    } else if (e.key === 'Escape') {
+        clearBtn.click();
+    } else if (e.key === 'Backspace') {
+        rmvBtn.click();
+    } else if (e.key === '.') {
+        decBtn.click();
+    }
+});
